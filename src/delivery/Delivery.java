324af6b218ce1cf75753d7e0b83a272101e31b96
taruc <taruc@B137>
2024-11-27 10:34:42 +0800

    complete

---
+++ src/delivery/Delivery.java
@@ -0,0 +1,13 @@
+package delivery;
+
+public class Delivery {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Happy");
+    }
+    
+    
+}
